c6d4805a1a4c8f57ffb5b98148e988d21cdd708b
vlabiitk <vlabiitk@gmail.com>
2024-10-08 15:09:39 +0530

    update simulator

---
+++ experiment/simulation/js/starting.js
@@ -0,0 +1,174 @@
+hviscometer.addEventListener("mouseenter",function(){
+    hovt.style.transitionDuration="0.5s"
+    hovt.style.visibility="visible"
+    hovt.innerText="VISCOMETER"
+    hovt.style.top="16.5%"
+    hovt.style.left="14%"
+    visco.style.transitionDuration="0.3s"
+    visco.style.scale="1.1"
+})
+
+hmachine.addEventListener("mouseenter",function(){
+    hovt.style.transitionDuration="0.5s"
+    hovt.style.visibility="visible"
+    hovt.innerText="WEIGHING MACHINE"
+    hovt.style.top="45.5%"
+    hovt.style.left="24%"
+    mach.style.transitionDuration="0.3s"
+    mach.style.scale="1.1"
+
+})
+
+hfunnel.addEventListener("mouseenter",function(){
+    hovt.style.transitionDuration="0.5s"
+    hovt.style.visibility="visible"
+    hovt.innerText="FUNNEL"
+    hovt.style.top="68%"
+    hovt.style.left="46%"
+    fun.style.transitionDuration="0.3s"
+    fun.style.scale="1.1"
+
+})
+
+hspatula.addEventListener("mouseenter",function(){
+    hovt.style.transitionDuration="0.5s"
+    hovt.style.visibility="visible"
+    hovt.innerText="SPATULA"
+    hovt.style.top="65.5%"
+    hovt.style.left="38%"
+    spat.style.transitionDuration="0.3s"
+    spat.style.scale="1.1"
+
+})
+
+hpdish.addEventListener("mouseenter",function(){
+    hovt.style.transitionDuration="0.5s"
+    hovt.style.visibility="visible"
+    hovt.innerText="PETRI DISH"
+    hovt.style.top="51.5%"
+    hovt.style.left="37%"
+    pdish.style.transitionDuration="0.3s"
+    pdish.style.scale="1.1"
+    pow.style.transitionDuration="0.3s"
+    pow.style.scale="1.1"
+
+})
+
+hpow.addEventListener("mouseenter",function(){
+    hovt.style.transitionDuration="0.5s"
+    hovt.style.visibility="visible"
+    hovt.innerText="SUGAR POWDER"
+    hovt.style.top="51.5%"
+    hovt.style.left="37%"
+    pdish.style.transitionDuration="0.3s"
+    pdish.style.scale="1.1"
+    pow.style.transitionDuration="0.3s"
+    pow.style.scale="1.1"
+
+})
+
+heflask1.addEventListener("mouseenter",function(){
+    hovt.style.transitionDuration="0.5s"
+    hovt.style.visibility="visible"
+    hovt.innerText="CONICAL FLASK"
+    hovt.style.top="38.5%"
+    hovt.style.left="41%"
+    eflask1.style.transitionDuration="0.3s"
+    eflask1.style.scale="1.1"
+
+})
+
+hwbeaker.addEventListener("mouseenter",function(){
+    hovt.style.transitionDuration="0.5s"
+    hovt.style.visibility="visible"
+    hovt.innerText="BEAKER WITH DISTILLED WATER"
+    hovt.style.top="38.5%"
+    hovt.style.left="45%"
+    wbeak.style.transitionDuration="0.3s"
+    wbeak.style.scale="1.1"
+
+})
+
+hbottle.addEventListener("mouseenter",function(){
+    hovt.style.transitionDuration="0.5s"
+    hovt.style.visibility="visible"
+    hovt.innerText="PYCNOMETER"
+    hovt.style.top="45%"
+    hovt.style.left="60%"
+    bot.style.transitionDuration="0.3s"
+    bot.style.scale="1.1"
+
+})
+
+hpip.addEventListener("mouseenter",function(){
+    hovt.style.transitionDuration="0.5s"
+    hovt.style.visibility="visible"
+    hovt.innerText="PIPPETE"
+    hovt.style.top="65.5%"
+    hovt.style.left="55%"
+    pip1.style.transitionDuration="0.3s"
+    pip1.style.scale="1.1"
+
+})
+
+hviscometer.addEventListener("mouseout",function(){
+    hovt.style.transitionDuration="0s"
+    hovt.style.visibility="hidden"
+    visco.style.scale="1.0"
+})
+
+hmachine.addEventListener("mouseout",function(){
+    hovt.style.transitionDuration="0s"
+    hovt.style.visibility="hidden"
+    mach.style.scale="1.0"
+})
+
+hfunnel.addEventListener("mouseout",function(){
+    hovt.style.transitionDuration="0s"
+    hovt.style.visibility="hidden"
+    fun.style.scale="1.0"
+})
+
+hspatula.addEventListener("mouseout",function(){
+    hovt.style.transitionDuration="0s"
+    hovt.style.visibility="hidden"
+    spat.style.scale="1.0"
+})
+
+hpdish.addEventListener("mouseout",function(){
+    hovt.style.transitionDuration="0s"
+    hovt.style.visibility="hidden"
+    pdish.style.scale="1.0"
+    pow.style.scale="1.0"
+})
+
+hpow.addEventListener("mouseout",function(){
+    hovt.style.transitionDuration="0s"
+    hovt.style.visibility="hidden"
+    pdish.style.scale="1.0"
+    pow.style.scale="1.0"
+})
+
+heflask1.addEventListener("mouseout",function(){
+    hovt.style.transitionDuration="0s"
+    hovt.style.visibility="hidden"
+    eflask1.style.scale="1.0"
+})
+
+hwbeaker.addEventListener("mouseout",function(){
+    hovt.style.transitionDuration="0s"
+    hovt.style.visibility="hidden"
+    wbeak.style.scale="1.0"
+})
+
+hbottle.addEventListener("mouseout",function(){
+    hovt.style.transitionDuration="0s"
+    hovt.style.visibility="hidden"
+    bot.style.scale="1.0"
+})
+
+hpip.addEventListener("mouseout",function(){
+    hovt.style.transitionDuration="0s"
+    hovt.style.visibility="hidden"
+    pip1.style.scale="1.0"
+})
